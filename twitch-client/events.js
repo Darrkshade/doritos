89dca6d9e96f40c1266068fcb0e04038238ef537
Daniel Davies <daniel.davies1994@hotmail.co.uk>
2020-06-30 09:05:39 +0100

    Changed sub message

--- twitch-client/events.js
+++ twitch-client/events.js
@@ -2,37 +2,37 @@
 const client = require("./connection.js");
 
 client.on("message", (channel, userstate, message, self) => {
-	console.log(
-		`Channel: ${channel}, Userstate: ${userstate.username}: Message: ${message}`
-	);
+  console.log(
+    `Channel: ${channel}, Userstate: ${userstate.username}: Message: ${message}`
+  );
 });
 
 client.on("resub", (channel, username, months, message, userstate, methods) => {
-	console.log("RESUBBBBBBBBBBB");
-	console.log("RESUBBBBBBBBBBB");
-	console.log("RESUBBBBBBBBBBB");
-	console.log("RESUBBBBBBBBBBB");
-	// Do your stuff.
-	console.log("----CHANNEL-----");
-	console.log(channel);
-	console.log("----USERNAME-----");
-	console.log(username);
-	console.log("----months-----");
-	console.log(userstate["msg-param-cumulative-months"]);
-	console.log("----message-----");
-	console.log(message);
-	console.log("----userstate-----");
-	console.log(userstate);
-	console.log("----endstate-----");
-	console.log("----6-----");
-	console.log(methods);
+  console.log("RESUBBBBBBBBBBB");
+  console.log("RESUBBBBBBBBBBB");
+  console.log("RESUBBBBBBBBBBB");
+  console.log("RESUBBBBBBBBBBB");
+  // Do your stuff.
+  console.log("----CHANNEL-----");
+  console.log(channel);
+  console.log("----USERNAME-----");
+  console.log(username);
+  console.log("----months-----");
+  console.log(userstate["msg-param-cumulative-months"]);
+  console.log("----message-----");
+  console.log(message);
+  console.log("----userstate-----");
+  console.log(userstate);
+  console.log("----endstate-----");
+  console.log("----6-----");
+  console.log(methods);
 
-	let emoji = "";
+  let emoji = "";
 
-	if (channel == "#pandatv") {
-		emoji = "dancepls";
-	}
+  if (channel == "#pandatv") {
+    emoji = "dancepls";
+  }
 
-	let messageToSend = `/me ${emoji} Welcome back ${username}! ${emoji}, Thank you for your ${userstate["msg-param-cumulative-months"]}`;
-	client.say(channel, messageToSend);
+  let messageToSend = `/me ${emoji} Welcome back ${username}! ${emoji} , Thank you for your ${userstate["msg-param-cumulative-months"]} Months`;
+  client.say(channel, messageToSend);
 });
